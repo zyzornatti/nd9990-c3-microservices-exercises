986bba4aed38ee9f41c58e7850d1660fe59c8cdd
zyzornatti <tobimustapha53@gmail.com>
2022-10-04 11:41:06 +0100

    done

--- lesson-2-containers/exercises/simple-node/server.js
+++ lesson-2-containers/exercises/simple-node/server.js
@@ -1,10 +1,13 @@
+const FAVORITE_NAME = process.env.NAME;
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
   while(true) {
-    console.log('Containers rule!');
+    //console.log('Containers rule!');
+	console.log(FAVORITE_NAME+' rules!');
     await sleep(5000);
   }
 }
